998ddb1b803ca3797bd6d010f7a738fc1481582a
xing <yyxingying@qq.com>
2026-01-16 16:35:38 +0800

    Initial Commit

---
+++ moleme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  moleme
+//
+//  Created by qin on 2026/1/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ moleme/molemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  molemeApp.swift
+//  moleme
+//
+//  Created by qin on 2026/1/16.
+//
+
+import SwiftUI
+
+@main
+struct molemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
